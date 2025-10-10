108653920db9259ad095cb6194de09b62d05e12a
PoolIllahuamanCostillo <pool10illa@gmail.com>
2025-10-10 12:25:55 +0200

    Tema: Métodos (I): firma, retorno, void; parámetros y paso por valor
    Se resolvió las actividades que están dentro de este tema

--- src/TrabajandoConMetodos.java
+++ src/TrabajandoConMetodos.java
@@ -11,8 +11,8 @@ public class TrabajandoConMetodos {
         // 1. Llama al método saludar() de la clase DemoMetodos.
         // Tu código aquí ↓
 
-           
-
+            method.saludar();
+        
         // ================================
         // EJERCICIO 2: Métodos con retorno
         // ================================
@@ -21,7 +21,11 @@ public class TrabajandoConMetodos {
         // 3. Llama al método presentar("Lucía") e imprime el resultado.
         // Tu código aquí ↓
     
-
+            /* 1.- */ int resultado = method.sumar(5, 3);
+            /* 2.- */ System.out.println("El resultado es: " + resultado);
+            /* 3.- */ String saludo = method.presentar("Lucía");
+            /* 4.- */ System.out.println(saludo);
+    
 
         // ================================
         // EJERCICIO 3: Paso por valor (primitivos)
@@ -30,6 +34,11 @@ public class TrabajandoConMetodos {
         // 2. Llama a method.incrementar(n).
         // 3. Imprime n después de llamar al método.
         // Tu código aquí ↓
+
+            int n = 10;
+            method.incrementar(n);
+            System.out.println(n);
+
 
 
         // ================================
@@ -41,26 +50,42 @@ public class TrabajandoConMetodos {
         // 3. Imprime la canción después.
         // Tu código aquí ↓
 
+            Cancion c = new Cancion("Original","Pepito");
+            method.cambiarTitulo(c);
+            System.out.println("La canción nueva es: " + c);
+
     }
 
 
 }
 
-
-
 // ================================
 // RESPUESTAS DE REFLEXIÓN
 // ================================
 // Ejercicio 1: ¿Qué ocurre al llamar a un método void como saludar()?
-// Respuesta: ....................................................
+// Respuesta: Al llamar el método void, este se ejecuta pero no devuelve ningún dato.
 
 // Ejercicio 2: ¿Qué diferencia hay entre un método void y uno con retorno?
-// Respuesta: ....................................................
+// Respuesta: Que el método void no devuelve ningún valor, mientras que uno con retorno
+//            devuelve el valor del tipo de dato usado al declarar variables.
 
 // Ejercicio 3: ¿Ha cambiado la variable n fuera del método incrementar()?
 // ¿Por qué?
-// Respuesta: ....................................................
+// Respuesta: No, la variable no cambia porque es un tipo primitivo y este solo recibe la copia del valor,
+//            pero no es modificable la variable original. Es por eso que dentro del método incrementar() de la clase "DemoMetodos"
+//            se incrementa e imprime 6, pero el valor original que está fuera del método de la clase "TrabajandoConMetodos"
+//            sigue siendo 5.
+
+
+// EXPLICACION PERSONAL:
+//ya que el método incrementar() dentro de la clase DemoMetodo copia el valor y se modifica imprimiendo 6,
+// pero no cambia el valor original que está fuera del método de la clase TrabajandoConMetodo (valor original es 5)                   
 
 // Ejercicio 4: ¿Qué ha ocurrido con el título de la Cancion al usar cambiarTitulo()?
 // ¿Por qué es diferente al caso del int?
-// Respuesta: ....................................................
+// Respuesta: Que al usar el método "cambiarTitulo" en el objeto canción, este recibe
+//            una referencia al objeto, lo que permite modificar directamente sus atributos.
+//            Es por eso que cambia de título "Original" a "Canción modificada"
+
+// Respuesta: Es diferente al caso del int ya que este son datos simples y al pasar a un método, 
+//            este realiza una copia del valor y trabaja con ella, mas no con la variable original.
