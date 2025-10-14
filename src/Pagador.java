80c6d2eac8e2e1833f165218eaf1fa54a92b9ed8
PoolIllahuamanCostillo <pool10illa@gmail.com>
2025-10-14 10:02:44 +0200

    Tema: Sobrecarga de métodos
    Se realizó todos los ejercicios de la actividad sobre este tema

--- src/Pagador.java
+++ src/Pagador.java
@@ -1,11 +1,10 @@
-package profesor;
-
 import utilidades.Pago;
 
 public class Pagador {
     public static void main(String[] args) {
         // Elige si trabajas con Pago o con Mensajero
         // Aquí un ejemplo con Pago:
+        
 
         Pago pago = new Pago(2000);
 
@@ -16,6 +15,8 @@ public class Pagador {
         // 2. Observa qué se imprime.
         // Tu código aquí ↓
 
+        pago.pagar(2000);
+
 
         // ================================
         // EJERCICIO 2: Pagar con tarjeta
@@ -24,7 +25,8 @@ public class Pagador {
         // 2. Imprime el resultado.
         // Tu código aquí ↓
 
-
+        pago.pagar(2000, "xd");
+        
         // ================================
         // EJERCICIO 3: Pagar con Bizum
         // ================================
@@ -32,22 +34,27 @@ public class Pagador {
         // 2. Imprime el resultado.
         // Tu código aquí ↓
 
+        pago.pagar(2000, 5770507);
+        
+
 
     }
+
 }
 
 // ================================
 // RESPUESTAS DE REFLEXIÓN
 // ================================
 // Pregunta 1: ¿Qué información mínima (parámetros) se necesita para realizar un pago?
-// Respuesta: ....................................................
+// Respuesta: Se necesita el parámetro de la cantidad a pagar del tipo de dato "double".
 
 // Pregunta 2: ¿En qué se diferencia el pago con tarjeta respecto al pago en efectivo?
-// Respuesta: ....................................................
+// Respuesta: El pago en efectivo se requiere solo la cantidad mientras que el pago con tarjeta se requiere la cantidad más el PIN.
 
 // Pregunta 3: ¿Por qué es útil que todos se llamen "pagar"?
-// Respuesta: ....................................................
+// Respuesta: Porque todos realizan la misma acción lógica y usar el mismo nombre hace que la API sea más coherente. 
 
 // Pregunta 4: ¿Qué pasaría si en lugar de pagar() hubiera métodos llamados pagarEfectivo(), pagarTarjeta(), pagarBizum()?
 // ¿Qué ventaja aporta la sobrecarga de métodos?
-// Respuesta: ....................................................ajerondo una cantidad.ng) pasando cantidad y un pin. pasando cantidad y un número de teléfono.os) se necesita para realizar un pago?................. tarjeta respecto al pago en efectivo?.................men "pagar"?.................() hubiera métodos llamados pagarEfectivo(), pagarTarjeta(), pagarBizum()?.................
+// Respuesta: El resultado sería el mismo, pero el código es más largo y menos legible al recordar diversos nombres distintos, 
+//            a diferencia del uso del método con sobrecarga que lo hace más corto, claro y conciso.
--- src/utilidades/Pago.java
+++ src/utilidades/Pago.java
@@ -2,18 +2,22 @@ package utilidades;
 
 public class Pago {
 
+    public Pago(int i) {
+        //TODO Auto-generated constructor stub
+    }
+
     // Pagar en efectivo
     public void pagar(double cantidad) {
-        System.out.println("Pagaste " + cantidad + "€ en efectivo.");
+        System.out.println("Pagaste " + cantidad + " euros en efectivo.");
     }
 
     // Pagar con tarjeta (necesita pin)
     public void pagar(double cantidad, String pin) {
-        System.out.println("Pagaste " + cantidad + "€ con tarjeta usando pin: " + pin);
+        System.out.println("Pagaste " + cantidad + " euros con tarjeta usando pin: " + pin);
     }
 
     // Pagar con Bizum (necesita teléfono)
     public void pagar(double cantidad, int telefono) {
-        System.out.println("Pagaste " + cantidad + "€ con Bizum al número: " + telefono);
+        System.out.println("Pagaste " + cantidad + " euros con Bizum al número: " + telefono);
     }
 }
